c0ab41056174265aa38baed7bc3ea544a13a5909
DSaini-10 <156831967+DSaini-10@users.noreply.github.com>
2024-06-24 03:12:30 +0530

    Time: 12 ms (90.30%) | Memory: 20 MB (70.83%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+class MinStack {
+public:
+    vector<pair <int,int>> vp;
+    MinStack() {
+    }
+    
+    void push(int val) {
+        if(vp.size()==0){
+            vp.emplace_back(make_pair(val,val));
+        }
+        else{
+            if((*(--vp.end())).second > val){
+                vp.emplace_back(make_pair(val,val));
+            }
+            else{
+                vp.emplace_back(make_pair(val,(*(--vp.end())).second));
+            }
+        }
+    }
+    
+    void pop() {
+        vp.erase(vp.end());
+    }
+    
+    int top() {
+        int x=(*(--vp.end())).first;
+        return x;
+    }
+    
+    int getMin() {
+        int y=(*(--vp.end())).second;
+        return y;
+        
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
